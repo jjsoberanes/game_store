d3651c133c8bc0fd0245e5221e7f8d42e8e3146a
kevinmalaverr <kevinmalaverr@gmail.com>
2021-01-21 20:50:10 -0500

    scroll function

--- js/actions.js
+++ js/actions.js
@@ -8,3 +8,32 @@ $toogleTheme.addEventListener('click', () => {
     document.documentElement.className = 'light-theme';
   }
 });
+
+/**
+ * @param {string} elementId
+ * @param {string} direction
+ */
+function scrollProducts(elementId, direction) {
+  const $section = document.getElementById(elementId);
+  const actualScroll = $section.scrollLeft;
+  const numberOfColumns = document.documentElement.clientWidth > 768 ? 4 : 2;
+  const scrollSize = $section.clientWidth / numberOfColumns;
+
+  switch (direction) {
+    case 'left':
+      $section.scroll({
+        left: actualScroll - scrollSize - 16,
+        behavior: 'smooth',
+      });
+      break;
+    case 'right':
+      $section.scroll({
+        left: actualScroll + scrollSize + 16,
+        behavior: 'smooth',
+      });
+      break;
+
+    default:
+      break;
+  }
+}
